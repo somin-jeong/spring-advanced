c05cf00db7177aa402b514252366d59e867a5dff
정소민 <yjk70702002@gmail.com>
2025-04-18 00:06:15 +0900

    feat: Lv 1-3. Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +15,7 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Size(min = 8)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$", message = "비밀번호는 8자 이상이며 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
