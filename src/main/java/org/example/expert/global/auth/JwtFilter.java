5566bd2077228dedf28a136f6af5b9d90e19976f
정소민 <yjk70702002@gmail.com>
2025-04-20 03:30:50 +0900

    refactor: 필터에서 발생하는 예외에 관한 JSON 응답 생성

--- src/main/java/org/example/expert/global/auth/JwtFilter.java
+++ src/main/java/org/example/expert/global/auth/JwtFilter.java
@@ -1,5 +1,6 @@
 package org.example.expert.global.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
@@ -11,8 +12,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -41,7 +45,7 @@ public class JwtFilter implements Filter {
 
         if (bearerJwt == null) {
             // 토큰이 없는 경우 400을 반환합니다.
-            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "JWT 토큰이 필요합니다.");
+            writeError(httpResponse, HttpStatus.UNAUTHORIZED, "JWT 토큰이 필요합니다.");
             return;
         }
 
@@ -51,7 +55,7 @@ public class JwtFilter implements Filter {
             // JWT 유효성 검사와 claims 추출
             Claims claims = jwtUtil.extractClaims(jwt);
             if (claims == null) {
-                httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 JWT 토큰입니다.");
+                writeError(httpResponse, HttpStatus.UNAUTHORIZED, "잘못된 JWT 토큰입니다.");
                 return;
             }
 
@@ -64,7 +68,7 @@ public class JwtFilter implements Filter {
             if (url.startsWith("/admin")) {
                 // 관리자 권한이 없는 경우 403을 반환합니다.
                 if (!UserRole.ADMIN.equals(userRole)) {
-                    httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자 권한이 없습니다.");
+                    writeError(httpResponse, HttpStatus.UNAUTHORIZED, "관리자 권한이 없습니다.");
                     return;
                 }
                 chain.doFilter(request, response);
@@ -74,17 +78,36 @@ public class JwtFilter implements Filter {
             chain.doFilter(request, response);
         } catch (SecurityException | MalformedJwtException e) {
             log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+            writeError(httpResponse, HttpStatus.UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
             log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
+            writeError(httpResponse, HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다.");
         } catch (UnsupportedJwtException e) {
             log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
+            writeError(httpResponse, HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 토큰입니다.");
         } catch (Exception e) {
             log.error("Invalid JWT token, 유효하지 않는 JWT 토큰 입니다.", e);
-            httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "유효하지 않는 JWT 토큰입니다.");
+            writeError(httpResponse, HttpStatus.UNAUTHORIZED, "유효하지 않는 JWT 토큰입니다.");
         }
+    }
+
+    public static void writeError(HttpServletResponse response, HttpStatus status, String message) throws IOException {
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        Map<String, Object> error = Map.of(
+                "status", status.name(),
+                "code", status.value(),
+                "message", message
+        );
+
+        // JSON 응답 작성
+        PrintWriter writer = response.getWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        writer.write(mapper.writeValueAsString(error));
+        writer.flush();
+        writer.close();
     }
 
     @Override
